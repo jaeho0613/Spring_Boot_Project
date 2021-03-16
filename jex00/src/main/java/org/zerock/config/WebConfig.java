501800d4abd64db4b8bab78d0f5641c7dd094a1c
Jaeho Jeong <ywm2004@gmail.com>
2021-03-16 21:42:04 +0900

    # [  P53 Java를 이용한 프로젝트 설정 ]

---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+// Web.xml을 대체하는 클래스
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
