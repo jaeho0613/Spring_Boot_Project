85cbba5dded38fe6847c52e3c936c269caa5c098
Jaeho Jeong <ywm2004@gmail.com>
2021-03-17 21:27:33 +0900

    # [ Part 2 -  스프링  MVC 설정 ] - Java를 이용한 Spring Project : jex01 - Web을 이용한 Spring Project : ex01

--- ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -14,7 +14,7 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class TimeMapperTests {
-
+	
 	@Setter(onMethod_ = @Autowired)
 	private TimeMapper timeMapper;
 
