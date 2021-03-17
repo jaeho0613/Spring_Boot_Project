47a3d1d31c55fb62657fe7d3fabf814d393c8c54
Jaeho Jeong <ywm2004@gmail.com>
2021-03-17 16:48:13 +0900

    # [ Part 1. 종료 ] - JDBC 연결 - Log 설정

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -20,8 +20,15 @@ public class RootConfig {
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.dirver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		
+		// log4j
+		// hikariConfig.setDriverClassName("oracle.jdbc.dirver.OracleDriver");
+		// hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		
+		// log4j2
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+		
 		hikariConfig.setUsername("book_ex");
 		hikariConfig.setPassword("book_ex");
 		
