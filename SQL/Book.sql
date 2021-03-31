b70d0d7eb35b8a8a0b37429634683243a61f2210
Jaeho Jeong <ywm2004@gmail.com>
2021-03-31 10:31:31 +0900

    # [ Chapter 25. 시작전 SQL tbl_attach 추가 ]

--- SQL/Book.sql
+++ SQL/Book.sql
@@ -1,4 +1,4 @@
---desc user_objects;
+-- desc user_objects;
 select object_name, object_type from user_objects;
 
 drop sequence seq_board;
@@ -6,10 +6,11 @@ drop sequence seq_reply;
 
 drop table tbl_board;
 drop table tbl_reply;
+drop table tbl_attach;
 
 COMMIT;
 
-CREATE SEQUENCE seq_board;
+create sequence seq_board;
 
 create sequence seq_reply;
 
@@ -31,9 +32,23 @@ create table tbl_reply (
     updateDate date default sysdate
 );
 
-ALTER TABLE tbl_board ADD CONSTRAINT pk_board PRIMARY KEY ( bno );
+create table tbl_attach (
+    uuid varchar2(100) not null,
+    uploadPath varchar2(200) not null,
+    fileName varchar2(100) not null,
+    fileType char(1) default 'I',
+    bno number(10,0)
+);
+
+alter table tbl_attach add constraint pk_attach primary key (uuid);
+alter table tbl_attach add constraint fk_board_attach foreign key (bno) references tbl_board(bno);
+
+alter table tbl_board add constraint pk_board primary key ( bno );
 alter table tbl_reply add constraint pk_reply primary key (rno);
 alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board(bno);
+
+commit;
+
 create index idx_reply on tbl_reply (bno desc, rno asc);
 
 insert into tbl_board(bno, title, content, writer) values(seq_board.nextval, 'Test', 'Test', 'Test');
